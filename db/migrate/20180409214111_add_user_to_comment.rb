9a2eee4adc7c5947c8348aff03b6e2264ebcb21f
Abraham Williams + Pearl Latteier <dev+abraham+pearlbea@bendyworks.com>
2018-04-09 17:10:31 -0500

    Remove extra Comment attributes

--- db/migrate/20180409214111_add_user_to_comment.rb
+++ db/migrate/20180409214111_add_user_to_comment.rb
@@ -3,5 +3,7 @@ class AddUserToComment < ActiveRecord::Migration[5.1]
     add_reference :comments, :user, foreign_key: true
     remove_column :comments, :by, :string
     remove_column :comments, :time, :datetime
+    remove_column :comments, :title, :string
+    remove_column :comments, :url, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,6 @@ ActiveRecord::Schema.define(version: 20180409214111) do
     t.integer "story_id"
     t.integer "comment_id"
     t.text "text"
-    t.string "url"
-    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
