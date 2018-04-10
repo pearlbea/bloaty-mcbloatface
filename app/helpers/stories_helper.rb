3ba21f11f40ed31e45ae9b1e9e5c18b7fac0040e
Pearl <pearlbea@gmail.com>
2018-04-10 11:39:56 -0500

    Add basic story view with comments

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -4,4 +4,18 @@ module StoriesHelper
   def host_name(url)
     URI.parse(url).host if url.present?
   end
+
+  def display_comments(comments)
+    obj = Hash.new('obj')
+    obj['top'] = []
+    comments.each do |comment|
+      if comment.comment_id.nil?
+        obj['top'] << comment
+      else
+        obj[comment.comment_id] = [] unless obj.key?(comment.comment_id)
+        obj[comment.comment_id] << comment
+      end
+    end
+    obj
+  end
 end
