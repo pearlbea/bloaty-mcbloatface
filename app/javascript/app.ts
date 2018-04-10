37658e19fab62997925cba91c47d5f20063a8e34
Pearl <pearlbea@gmail.com>
2018-04-10 15:04:30 -0500

    Add header navigation on scroll

--- app/javascript/app.ts
+++ app/javascript/app.ts
@@ -1,9 +1,33 @@
 import { Story } from "./story";
 
 export class App {
+  private readonly HEADER = ".navbar";
+
   public init() {
-    console.log("initing app");
+    this.initStory();
+    this.addEventListeners();
+  }
+
+  initStory(): void {
     const story = new Story();
     story.init();
+  }
+
+  get header() {
+    return document.querySelector(this.HEADER);
+  }
+
+  addEventListeners(): void {
+    window.addEventListener("scroll", this.handleScroll.bind(this));
+  }
+
+  handleScroll(): void {
+    if (window.pageYOffset > 100) {
+      this.header.classList.add("shrink");
+      this.header.classList.remove("grow");
+    } else {
+      this.header.classList.remove("shrink");
+      this.header.classList.add("grow");
+    }
   }
 }
