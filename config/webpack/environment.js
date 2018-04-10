b97a7b9ac7692c50f726b2b7d96868d1d05f92e2
Abraham Williams <4braham@gmail.com>
2018-04-10 17:41:14 -0500

    Move html to loader and silence Angular warning

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,16 @@
-const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
+const webpack = require('webpack')
+const { resolve } = require('path')
+const { config, environment } = require('@rails/webpacker')
+const typescript = require('./loaders/typescript')
+const html = require('./loaders/html')
 
 environment.loaders.append('typescript', typescript)
-environment.loaders.append('html', {
-  test: /\.html$/,
-  use: [{
-    loader: 'html-loader',
-    options: {
-      minimize: true,
-      removeAttributeQuotes: false,
-      caseSensitive: true,
-      customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
-      customAttrAssign: [ /\)?\]?=/ ]
-    }
-  }]
-})
+environment.loaders.append('html', html)
+environment.plugins.append('ContextReplacement',
+  new webpack.ContextReplacementPlugin(
+    /angular(\\|\/)core(\\|\/)(@angular|esm5)/,
+    resolve(config.source_path)
+  )
+)
 
 module.exports = environment
