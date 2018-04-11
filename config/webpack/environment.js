4e33620669e6781b12ab71bf7813d18009024e1c
Abraham Williams <4braham@gmail.com>
2018-04-11 16:16:48 -0500

    Add workbox

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,7 @@ const { resolve } = require('path')
 const { config, environment } = require('@rails/webpacker')
 const typescript = require('./loaders/typescript')
 const html = require('./loaders/html')
+const workbox = require('workbox-webpack-plugin');
 
 environment.loaders.append('typescript', typescript)
 environment.loaders.append('html', html)
@@ -11,6 +12,13 @@ environment.plugins.append('ContextReplacement',
     /angular(\\|\/)core(\\|\/)(@angular|esm5)/,
     resolve(config.source_path)
   )
+)
+environment.plugins.append('workbox',
+  new workbox.GenerateSW({
+    swDest: resolve('public', 'sw.js'),
+    clientsClaim: true,
+    skipWaiting: true,
+  })
 )
 
 module.exports = environment
