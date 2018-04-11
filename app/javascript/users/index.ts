bc4edb9bc18eb6c4756b04efb5e7fe0eb412f9ba
Pearl <pearlbea@gmail.com>
2018-04-11 17:02:32 -0500

    Make Angular happy

--- app/javascript/users/index.ts
+++ app/javascript/users/index.ts
@@ -1,11 +1,13 @@
-import './polyfills.ts';
+import "./polyfills.ts";
 
-import '../theme';
-import './users';
+import "../theme";
+import "./users";
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule);
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector("app-users")) {
+    platformBrowserDynamic().bootstrapModule(AppModule);
+  }
 });
