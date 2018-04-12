5faadaa92d6fa027734d40d84ee306ee4f7e093d
Abraham Williams <4braham@gmail.com>
2018-04-12 14:37:33 -0500

    Service worker for /users

--- config/webpack/plugins/workbox.js
+++ config/webpack/plugins/workbox.js
@@ -1,8 +1,7 @@
 const { resolve } = require('path');
 const workbox = require('workbox-webpack-plugin');
 
-module.exports = new workbox.GenerateSW({
+module.exports = new workbox.InjectManifest({
+  swSrc: resolve('app', 'javascript', 'sw.js'),
   swDest: resolve('public', 'sw.js'),
-  clientsClaim: true,
-  skipWaiting: true,
 });
