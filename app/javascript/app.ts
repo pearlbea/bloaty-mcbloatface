246ffa65bed773bd3fcf905d2c6ae9112c7468de
Abraham Williams + Pearl Latteier <dev+abraham+pearlbea@bendyworks.com>
2018-04-13 15:23:47 -0500

    Move SW registration to application pack

--- app/javascript/app.ts
+++ app/javascript/app.ts
@@ -31,13 +31,3 @@ export class App {
     story.init();
   }
 }
-
-// if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/sw.js').then(registration => {
-//       console.log('SW registered: ', registration);
-//     }).catch(registrationError => {
-//       console.log('SW registration failed: ', registrationError);
-//     });
-//   });
-// }
--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -4,3 +4,13 @@ import { App } from "../app";
 document.addEventListener("DOMContentLoaded", () => {
   const app = new App().init();
 });
+
+// if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js').then(registration => {
+//       console.log('SW registered: ', registration);
+//     }).catch(registrationError => {
+//       console.log('SW registration failed: ', registrationError);
+//     });
+//   });
+// }
