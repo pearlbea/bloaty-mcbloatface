17d1122e33f414cd628660079e5db7f7fb51b981
Abraham Williams + Pearl Latteier <dev+abraham+pearlbea@bendyworks.com>
2018-04-13 15:15:27 -0500

    Move SW generation to production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,11 +2,9 @@ const { environment } = require('@rails/webpacker')
 const context = require('./plugins/context')
 const html = require('./loaders/html')
 const typescript = require('./loaders/typescript')
-const workbox = require('./plugins/workbox')
 
 environment.loaders.append('typescript', typescript)
 environment.loaders.append('html', html)
 environment.plugins.append('ContextReplacement', context)
-environment.plugins.append('workbox', workbox)
 
 module.exports = environment
