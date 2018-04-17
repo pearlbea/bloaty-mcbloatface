d713204b125667bae181cd6d996b6425e8bc6036
Pearl <pearlbea@gmail.com>
2018-04-17 16:52:52 -0400

    Add code comments for exercises and remove todo code

---
+++ app/javascript/packs/story.ts
@@ -0,0 +1,3 @@
+// * import Story from story.ts
+//   (for an example, see how packs/application.ts imports App from app.ts)
+// * create an instance of Story and call the 'init' method on that instance
