8b87c69f2c72d5476dcecd6dbe4365d24aabf1b4
Abraham Williams <4braham@gmail.com>
2018-04-18 11:45:33 -0400

    Move hero images to work offline

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,6 +1,15 @@
 require 'uri'
 
 module StoriesHelper
+  def random_cute_animal_image
+    [
+      'andrew-pons-9713-unsplash.jpg',
+      'freddie-marriage-40645-unsplash.jpg',
+      'josh-bean-591099-unsplash.jpg',
+      'matheus-queiroz-361095-unsplash.jpg'
+    ].sample
+  end
+
   def host_name(url)
     URI.parse(url).host if url.present?
   end
